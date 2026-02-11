da2b4f8af6563a78904c98c71f0859f21db99de1
arkanto99 <pablorodriguez.fernandez@usc.gal>
2026-02-11 09:57:55 +0100

    [ADD] granite-r2 launcher

--- launch_retrieval.sh
+++ launch_retrieval.sh
@@ -13,22 +13,26 @@ for i in {1..10}; do
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/all-minilm-l6-v2.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/qwen3-Embedding.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/gemma-300m.yaml --run-id $i
+    python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/granite-english-r2.yaml --run-id $i
 
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bm25_bge-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bge-m3_bge-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/all-minilm-l6-v2_bge-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/qwen3-Embedding_bge-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/gemma-300m_bge-reranker.yaml --run-id $i
+    python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/granite-english-r2_bge-reranker.yaml --run-id $i
 
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bm25_qwen-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bge-m3_qwen-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/all-minilm-l6-v2_qwen-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/qwen3-Embedding_qwen-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/gemma-300m_qwen-reranker.yaml --run-id $i
+    python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/granite-english-r2_qwen-reranker.yaml --run-id $i
 
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bm25_jina-v3-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/bge-m3_jina-v3-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/all-minilm-l6-v2_jina-v3-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/qwen3-Embedding_jina-v3-reranker.yaml --run-id $i
     python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/gemma-300m_jina-v3-reranker.yaml --run-id $i
+    python3 generate_testset.py --dataset $QUESTIONS --config $CONFIGS/granite-english-r2_jina-v3-reranker.yaml --run-id $i
 done
